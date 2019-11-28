74ce3a3d1340a374cb41f67ede442400af7353aa
Alexandre Alain <alexandre_alain_91@hotmail.com>
2019-11-28 15:14:36 -0500

    rajout user et PW, manque sudo/noPW/clé SSh

--- pine64.sh
+++ pine64.sh
@@ -3,7 +3,9 @@
 #IP
 
 #User
-
-useradd -m -p tge243 -s /bin/bash alexandre.alain1
+#Create user without PW
+useradd -m -p tge243 -s /bin/bash alexandre
+#Initiate passwd for the user
+echo -e "tge243\ntge243" | passwd alexandre
 
 #SSH

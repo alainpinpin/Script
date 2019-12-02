3f9b72ea648e2af42cbc5c80061cbbd11fba77c4
Alexandre Alain <alexandre_alain_91@hotmail.com>
2019-12-02 15:58:25 -0500

    supposé marcher reste a vérif apres merge et upload pine

--- pine64.sh
+++ pine64.sh
@@ -5,3 +5,9 @@
 #User
 
 #SSH
+	#pour modifier le fichier pour empecher le login SSH root trouver on the web
+echo "PermitRootLogin no" >> /etc/ssh/sshd_config
+	#Allow only alexandre to log in
+echo "AllowUsers alexandre" >> /etc/ssh/sshd_config
+	#Interdit le login par passwd
+echo "PasswordAuthentication no" >> /etc/ssh/sshd_config

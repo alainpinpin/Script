4d3c239750c58c7ee007fa1a7deb400d81cdb13e
Alexandre Alain <alexandre_alain_91@hotmail.com>
2019-12-02 14:59:48 -0500

    Version théoriquement fini, verifier dernier avec Julien

--- pine64.sh
+++ pine64.sh
@@ -9,7 +9,9 @@ useradd -m -p tge243 -s /bin/bash alexandre
 echo -e "tge243\ntge243" | passwd alexandre
 	#Add user to Sudoer
 usermod -aG sudo alexandre
-
-echo " " >> /etc/sudoers
+	#ligne pour permettre le root log in sans PW
+echo "alexandre     ALL=(ALL) NOPASSWD:ALL" >> /etc/sudoers
+	#Création de la SSH Key?? pas sur checker avec julien avant de runner
+ssh-keygen -t rsa -N "" -f my.key
 
 #SSH

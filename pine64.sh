a7c962a0b8a76077daaaad1cf7ecb73086441618
Alexandre Alain <alexandre_alain_91@hotmail.com>
2019-12-02 14:44:29 -0500

    switch pour francois manque ligne a mettre pour sudoers

--- pine64.sh
+++ pine64.sh
@@ -3,9 +3,13 @@
 #IP
 
 #User
-#Create user without PW
+	#Create user without PW
 useradd -m -p tge243 -s /bin/bash alexandre
-#Initiate passwd for the user
+	#Initiate passwd for the user
 echo -e "tge243\ntge243" | passwd alexandre
+	#Add user to Sudoer
+usermod -aG sudo alexandre
+
+echo " " >> /etc/sudoers
 
 #SSH

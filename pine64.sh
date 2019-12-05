4e230bce15ac1cd80c99c60358a306fdbd2077b9
Alexandre Alain <alexandre_alain_91@hotmail.com>
2019-12-05 14:50:02 -0500

    partie pret a merger et tester en script fini

--- pine64.sh
+++ pine64.sh
@@ -11,7 +11,12 @@ echo -e "tge243\ntge243" | passwd alexandre
 usermod -aG sudo alexandre
 	#ligne pour permettre le root log in sans PW
 echo "alexandre     ALL=(ALL) NOPASSWD:ALL" >> /etc/sudoers
-	#Création de la SSH Key?? pas sur checker avec julien avant de runner
-ssh-keygen -t rsa -N "" -f my.key
+	#Import de ma clé publique manuellement
+mkdir .ssh /home/alexandre/
+
+touch /home/alexandre/.ssh/authorized_keys
+
+echo "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCyzEEOwOeS/NCPsi3D0yYnZE3k60j/Zq0SgS8gknBCoPvPchRIfVtBMJA1tFMTDW/Pey/M1Iy5WjuU6UjtT93rHs833ijEBGjnUCPFK2DqwuJNpQYOrkF4Ja8S/QXpd/8lPK9b3tQLD1QYWQXLxG13CQXyHqj6zQQOVqTws9CdKkpiIvXrstslquYmeM3yhg99KQUkypyN5OSI9k/js/nOJk37f/mZawHEoyLg62mNJWU9cuoVD6FeQnJNzK4ocQxLjWmfH9k7zMcFnnkT6yrPuzQgsQrtGq6UbwKdVLiSMjRGnyTw+AV0k6lt0S4/rDSoavsaN3wQp5DjTlk8Fmwh alain pin pin@DESKTOP-U6LPDCQ
+" > /home/alexandre/.ssh/authorized_keys
 
 #SSH
